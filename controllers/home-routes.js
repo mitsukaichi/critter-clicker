df15892b1f8511b4f6257d84d4f4c019b8a5fbda
Janet Webster <twixmixy@gmail.com>
2023-11-07 16:07:42 -0500

    GET ROUTE to an individual petpic post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { Users, Posts, Index, Comments, Categories } = require('../models'); // may need to update based off models
 const withAuth = require('../utils/auth'); // may need to update based off utils
 
-// GET all petpic posts
+// GET ROUTE all petpic posts
 router.get('/', async (req, res) => {
     try {
         const petpicData = await Posts.findAll({
@@ -34,4 +34,38 @@ router.get('/', async (req, res) => {
         console.log(error);
         res.status(500).json(error);
     }
-})
+});
+
+// GET ROUTE to an individual petpic post
+router.get('/petpic/:id', withAuth, async (req, res) => {
+    try {
+        const petpicData = await Posts.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Users,
+                    attributes: ["username"],
+                },
+                {
+                    model: Comments,
+                    include: [User],
+                },
+                {
+                    model: Likes,
+                    attributes: ["like"],
+                },
+            ],
+        });
+
+        const petpicPost = petpicData.get({ plain: true });
+        console.log(petpicPost)
+
+        res.render('petpic', {
+            ...petpicPost,
+            logged_in: req.session.logged_in,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+        res.redirect('/login');
+    }
+});
