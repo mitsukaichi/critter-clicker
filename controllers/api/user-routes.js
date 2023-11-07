ca06dfbf8afe0aa58ad72aef9462d5d9f53f9db5
Janet Webster <twixmixy@gmail.com>
2023-11-07 17:04:00 -0500

    POST ROUTE to login to the website

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -16,4 +16,34 @@ router.post('/', async (req, res) => {
         console.log(error);
         res.status(400).json(error);
     }
-})
+});
+
+// POST ROUTE to login to the website
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await Users.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            console.log("Could not find that user");
+            res.status(400).json({ message: "Incorrect email. Please try again."});
+            return;
+        };
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            console.log("That password did not match our records");
+            res.status(400).json({ message: "Incorrect password. Please try again."});
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({ user: userData, message: "You have successfully logged in!"});
+        });
+    } catch (error) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
