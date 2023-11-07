0a2f1f9dfdee568d3c0995cf1206ccb38109ba47
Janet Webster <twixmixy@gmail.com>
2023-11-07 17:15:49 -0500

    POST ROUTE to create a new petpic post

--- controllers/api/petpic-routes.js
+++ controllers/api/petpic-routes.js
@@ -3,3 +3,18 @@ const router = require('express').Router();
 const { Users, Posts, Index, Comments, Categories, Likes } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// POST ROUTE to create a new petpic post
+router.post('/', withAuth, async (req, res) => {
+    console.log(req.body);
+    try {
+        const newPetPic = await Posts.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newPetPic);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error);
+    }
+});
