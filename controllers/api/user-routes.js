339bdd9468aa7860789780fa0caf00898f584aac
Janet Webster <twixmixy@gmail.com>
2023-11-07 16:52:40 -0500

    POST ROUTE to create a new user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,3 +2,18 @@
 const router = require('express').Router();
 const { Users, Posts, Index, Comments, Categories, Likes } = require('../../models');
 
+// POST ROUTE to create a new user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await Users.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error);
+    }
+})
