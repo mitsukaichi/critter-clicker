f3180bcb3e7bf7d2d992f98d0e2c4b4c654830dd
Janet Webster <twixmixy@gmail.com>
2023-11-07 17:42:13 -0500

    PUT and DEL just in case

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -39,4 +39,50 @@ router.get('/', async (req, res) => {
         console.log(error);
         res.status(500).json(error);
     }
-})
+});
+
+// PUT ROUTE to update a comment
+// most likely we will not use this!
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedComment = await Comment.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!updatedComment[0]) {
+            res.status(400).json({ message: "There is no comment with that ID"});
+            return;
+        }
+        console.log("Updated comment");
+        res.status(200).json(updatedComment);
+    } catch (error) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// DELETE ROUTE to delete a comment
+// most likely we will not use this!
+router.delete('/:id', async (req, res) => {
+    try {
+        const comment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!comment) {
+            res.status(404).json({ message: "There is no comment with that ID" });
+            return;
+        }
+        res.status(200).json(comment);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// EXPORT
+module.exports = router;
