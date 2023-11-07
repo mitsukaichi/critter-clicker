691692d229a8389813208af20128f71fe2539f70
Janet Webster <twixmixy@gmail.com>
2023-11-07 15:56:59 -0500

    GET all petpic posts route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,4 +3,35 @@ const router = require('express').Router();
 const { Users, Posts, Index, Comments, Categories } = require('../models'); // may need to update based off models
 const withAuth = require('../utils/auth'); // may need to update based off utils
 
+// GET all petpic posts
+router.get('/', async (req, res) => {
+    try {
+        const petpicData = await Posts.findAll({
+            include: [
+                {
+                    model: Users,
+                    attributes: ["username"],
+                },
+                {
+                    model: Comments,
+                    attributes: ["comment"],
+                },
+                {
+                    model: Likes,
+                    attributes: ["like"],
+                },
+            ],
+        });
 
+        const petpicPosts = petpicData.map((petpicPost) =>
+        petpicPost.get({ plain: true }));
+
+        res.render('homepage', {
+            petpicPosts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+})
