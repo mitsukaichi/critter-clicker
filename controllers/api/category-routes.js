a835616c37defdb31581b4a8ad876da03095653b
Janet Webster <twixmixy@gmail.com>
2023-11-07 17:29:39 -0500

    category routes imports

---
+++ controllers/api/category-routes.js
@@ -0,0 +1,5 @@
+// IMPORTS
+const router = require('express').Router();
+const { Users, Posts, Index, Comments, Categories, Likes } = require('../../models');
+const withAuth = require('../../utils/auth');
+
