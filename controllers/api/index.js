2e78be16a37f8c91087533b34012745028b3a479
Janet Webster <twixmixy@gmail.com>
2023-11-07 16:41:22 -0500

    API routes Index imports, middleware, export

---
+++ controllers/api/index.js
@@ -0,0 +1,15 @@
+// IMPORTS
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const petpicRoutes = require('./petpic-routes');
+const commentRoutes = require('./comment-routes');
+const likeRoutes = require('./like-routes');
+
+// MIDDLEWARE
+router.use('/users', userRoutes);
+router.use('/petpic', petpicRoutes);
+router.use('/comments', commentRoutes);
+router.use('/likes', likeRoutes);
+
+// EXPORT
+module.exports = router;
