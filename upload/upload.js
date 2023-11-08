f08f6252b483eeeb443950687d81054156b8bc0f
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-08 11:25:34 -0800

    configured cloudinary with env variables

--- upload/upload.js
+++ upload/upload.js
@@ -1,10 +1,14 @@
 // two libraries to upload image to cloudinary
 // Require the cloudinary library
 const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
 
-// Return "https" URLs by setting secure: true
+// Return "https" URLs by setting secure: true      
 cloudinary.config({
-  secure: trues
+    cloud_name: CLOUD_NAME,
+    api_key: CLOUD_KEY,
+    api_secret: CLOUD_SECRET,
+    secure: true
 });
 
 // Log the configuration
@@ -18,22 +22,22 @@ const uploadImage = async (imagePath) => {
     // Use the uploaded file's name as the asset's public ID and 
     // allow overwriting the asset with new versions
     const options = {
-      use_filename: true,
-      unique_filename: false,
-      overwrite: true,
+        use_filename: true,
+        unique_filename: false,
+        overwrite: true,
     };
 
     try {
-      // Upload the image
-      const result = await cloudinary.uploader.upload(imagePath, options);
-      console.log(result);
-      return result.public_id;
+        // Upload the image
+        const result = await cloudinary.uploader.upload(imagePath, options);
+        console.log(result);
+        return result.public_id;
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
 };
 
 // upload can be specified as a local path, a remote HTTP or HTTPS URL, a whitelisted storage bucket (S3 or Google Storage) URL, a data stream, a base64 data URI, or an FTP URL.
 cloudinary.v2.uploader
-.upload("/home/my_image.jpg")
-.then(result=>console.log(result));
+    .upload("/home/my_image.jpg")
+    .then(result => console.log(result));
