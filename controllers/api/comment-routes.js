c1ee01276a18b5ba0307e78ec24183bcfb60d9de
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-08 13:44:13 -0800

    changed attribute names

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
             include: [
                 {
                     model: Users,
-                    attributes: ["username"],
+                    attributes: ["users"],
                 },
                 {
                     model: Posts,
