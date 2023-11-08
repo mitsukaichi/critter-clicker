a22427277d27c0706808981431343aeb29487b11
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-08 14:16:14 -0800

    changed property name from post_id to posts_id in commentSeed

--- seeds/commentSeed.js
+++ seeds/commentSeed.js
@@ -3,32 +3,32 @@ const { Comments } = require('../models');
 const commentData = [
   {
     comment_text: "Wow! Nice dog!",
-    post_id: 3,
+    posts_id: 3,
     users_id: 1
   },
   {
     comment_text: "What a colorful bird!",
-    post_id: 1,
+    posts_id: 1,
     users_id: 4
   },
   {
     comment_text: "I love your cat!",
-    post_id: 4,
+    posts_id: 4,
     users_id: 2
   },
   {
     comment_text: "Your turtle is so cool!",
-    post_id: 4,
+    posts_id: 4,
     users_id: 3
   },
   {
     comment_text: "Those fish are so colorful!",
-    post_id: 5,
+    posts_id: 5,
     users_id: 5
   },
   {
     comment_text: "That is a big dog!",
-    post_id: 5,
+    posts_id: 5,
     users_id: 4
   },
 ];
