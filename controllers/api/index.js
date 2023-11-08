a9bd7f49c0b02f8f0a2be0967689d02e5a880146
Janet Webster <twixmixy@gmail.com>
2023-11-08 12:41:01 -0500

    Fixed the routes!

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,15 +3,15 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const petpicRoutes = require('./petpic-routes');
 const commentRoutes = require('./comment-routes');
-// const categoryRoutes = require('./category-routes');
-// const likeRoutes = require('./like-routes');
+const categoryRoutes = require('./category-routes');
+const likeRoutes = require('./like-routes');
 
 // MIDDLEWARE
 router.use('/users', userRoutes);
 router.use('/petpic', petpicRoutes);
 router.use('/comments', commentRoutes);
-// router.use('/categories', categoryRoutes);
-// router.use('/likes', likeRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/likes', likeRoutes);
 
 // EXPORT
 module.exports = router;
