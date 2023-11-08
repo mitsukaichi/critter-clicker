f09ff60ebf20cf2e807f0bcb2936c385672b15a9
Minami Itsukaichi <mitukaichi@gmail.com>
2023-11-08 15:33:51 -0800

    add function to dismiss modal

--- public/js/like-nosignin.js
+++ public/js/like-nosignin.js
@@ -4,4 +4,10 @@ const likeWithoutSigininHandler = (event) => {
     document.querySelector('.custom-logout-modal').classList.remove("hidden");
   };
 
+  const closeModal = (event) => {
+    document.querySelector('.custom-logout-modal').classList.add("hidden");
+  };
+  
+
   document.querySelector('.like-no-signin').addEventListener('click',likeWithoutSigininHandler);
+  document.getElementById('dismiss-modal').addEventListener('click', closeModal);
