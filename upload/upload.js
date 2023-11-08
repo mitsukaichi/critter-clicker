588456575c0b80553e21fd258117293c3aa4f027
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-08 11:17:54 -0800

    added test code to upload

--- upload/upload.js
+++ upload/upload.js
@@ -1,9 +1,37 @@
 // two libraries to upload image to cloudinary
-require('dotenv').config();
-
+// Require the cloudinary library
 const cloudinary = require('cloudinary').v2;
 
-console.log(cloudinary.config().cloud_name);
+// Return "https" URLs by setting secure: true
+cloudinary.config({
+  secure: trues
+});
+
+// Log the configuration
+console.log(cloudinary.config());
+
+/////////////////////////
+// Uploads an image file
+/////////////////////////
+const uploadImage = async (imagePath) => {
+
+    // Use the uploaded file's name as the asset's public ID and 
+    // allow overwriting the asset with new versions
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+    };
+
+    try {
+      // Upload the image
+      const result = await cloudinary.uploader.upload(imagePath, options);
+      console.log(result);
+      return result.public_id;
+    } catch (error) {
+      console.error(error);
+    }
+};
 
 // upload can be specified as a local path, a remote HTTP or HTTPS URL, a whitelisted storage bucket (S3 or Google Storage) URL, a data stream, a base64 data URI, or an FTP URL.
 cloudinary.v2.uploader
