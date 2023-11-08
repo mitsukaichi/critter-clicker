8b176305bbedae4aee5838b666fc0099f73d1249
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-11-08 10:52:15 -0800

    likes model

---
+++ models/Likes.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Likes extends Model {}
+
+Likes.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        },
+        liked: {
+            type: DataTypes.BOOLEAN,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'likes'
+    }
+)
