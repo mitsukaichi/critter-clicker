b137d34c250598f25f1c8234de677363460aae26
Janet Webster <twixmixy@gmail.com>
2023-11-10 23:14:19 -0500

    comment posting issue fixed

--- public/js/comment.js
+++ public/js/comment.js
@@ -5,14 +5,14 @@ async function newComment(event) {
 
     const comment_text = document.getElementById("comment").value.trim();
     const url = window.location.toString().split('/');
-    const Posts_id = url[url.length - 1];
+    const posts_id = url[url.length - 1];
     const error = document.getElementById('comment-error');
 
     if (comment_text) {
         const response = await fetch('/api/comments', {
             method: "POST",
             body: JSON.stringify({
-                Posts_id,
+                posts_id,
                 comment_text,
             }),
             headers: {
@@ -22,7 +22,7 @@ async function newComment(event) {
         console.log(comment_text);
         console.log(response);
         if (response.ok) {
-            // document.location.reload();
+            document.location.reload();
         } else {
             response.json()
             // .then(dataErr => {
